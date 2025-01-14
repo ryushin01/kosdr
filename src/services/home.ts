0a1a7f5b2cdbc297103c2c1f620c3b7b3b1a926c
zero.ryushin <zero.ryushin@bankle.co.kr>
2025-01-14 15:10:30 +0900

    fake rest api test

--- src/services/home.ts
+++ src/services/home.ts
@@ -1,19 +1,41 @@
 import { createAxiosInstance } from "@services";
 
-// interface getHomeDataList {
-//   title: string;
-//   body: string;
-//   userId: number;
+// interface PassengerDataProps {
+//   name: string;
+//   trips: number;
+//   airline: number;
 // }
 
 // GET
-export const getUserListData = () => createAxiosInstance.get("/users");
+export const getAirlinesListData = () => createAxiosInstance.get("/airlines");
 
 // GET with param
-export const getUserData = (id: number) => createAxiosInstance.get(`/users/${id}`);
+export const getAirlineData = (id: string) => createAxiosInstance.get(`/airlines/${id}`);
 
 // GET with multiple params
+export const getPassengerListData = ({
+                                       page = 1,
+                                       size = 10,
+                                     }: {
+  page: number,
+  size: number,
+}) => createAxiosInstance.get(`/passenger?page=${page}&size=${size}`);
 
+// POST
+export const postAirlineData = ({
+                                  _id, name, country, logo, slogan, head_quarters, website, established,
+                                }: {
+  _id: string,
+  name: string,
+  country: string,
+  logo: string,
+  slogan: string,
+  head_quarters: string,
+  website: string,
+  established: string,
+}) => createAxiosInstance.post("/airlines", {
+  _id, name, country, logo, slogan, head_quarters, website, established,
+});
 
 // POST
 // export const createHomeData = ({ title, body, userId }: homeDataProps) => createAxiosInstance.post("/posts", {
