6494a8c5ec232f19c34ecfb714e4fcf62080671b
zero.ryushin <zero.ryushin@bankle.co.kr>
2025-02-10 17:59:21 +0900

    20250210

--- src/components/error-boundary/Error.tsx
+++ src/components/error-boundary/Error.tsx
@@ -1,12 +1,19 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { useErrorBoundary } from "react-error-boundary";
 
 const Error = () => {
+  const router = useRouter();
   const { resetBoundary } = useErrorBoundary();
+
+  const toHomePage = () => router.push("/");
 
   return (
     <>
       <p>일시적인 오류가 발생했습니다.</p>
       <button type="button" onClick={resetBoundary}>새로고침</button>
+      <button type="button" onClick={toHomePage}>홈으로 이동하기</button>
     </>
   );
 };
